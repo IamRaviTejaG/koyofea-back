5e2d6181d39031165399588622a879bac4168040
ankit <ankitsocial16@gmail.com>
2018-07-18 16:08:15 +0530

    lint changes

--- config/express.js
+++ config/express.js
@@ -1,28 +1,24 @@
-const bodyparser = require("body-parser")
-const dotenv = require("dotenv").config()
-const express = require("express")
-const morgan = require('morgan')
-const cors = require('cors')
-import { auth } from "./auth"
-import { base } from "../routes"
-import { autofill } from "../routes/autofill"
-import { college } from "../routes/college"
-import { recruiter } from "../routes/recruiter"
-import { student } from "../routes/student"
+const bodyparser = require("body-parser");
+const dotenv = require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+import { auth } from "./auth";
+import { base } from "../routes";
+import { autofill } from "../routes/autofill";
+import { college } from "../routes/college";
+import { recruiter } from "../routes/recruiter";
+import { student } from "../routes/student";
 import { dashboard } from "../modules/common";
 import { query } from "./db";
 
-let app = express()
-app.use(cors())
-app.use(morgan('dev'))
-app.use(auth.initialize())
-app.use(bodyparser.json()) //for parsing application/json()
-app.use(bodyparser.urlencoded({extended: true}))
-app.use("/", base)
-app.use("/autofill", autofill)
-app.use("/college", college)
-app.use("/recruiter", recruiter)
-app.use("/student", student)
+let app = express();
+app.use(cors());
+app.use(morgan("dev"));
+app.use(auth.initialize());
+app.use(bodyparser.json()); //for parsing application/json()
+app.use(bodyparser.urlencoded({ extended: true }));
+
 // app.all("/" + "*", (req, res, next) => {
 //   return auth.authenticate((err, user, info) => {
 //     if(err) {return next(err)}
@@ -41,100 +37,115 @@ app.use("/student", student)
 // Middle ware for token
 app.all("/*", (req, res, next) => {
   if (
-    req.path == '/login'
-    || req.path == '/'
-    || req.path == '/favicon.ico'
-    || req.path == '/robots.txt'
-    || req.path == '/signup') {
+    req.path == "/login" ||
+    req.path == "/" ||
+    req.path == "/favicon.ico" ||
+    req.path == "/robots.txt" ||
+    req.path == "/signup"
+  ) {
     return next();
   }
-  let token = auth.decode_token(req.get('x-api-key'))
-  req.token_data = token
-  dashboard.user_data(req).then(data => {
-    req.basic_data = data
-    next()
-  }).catch(err => {
-    return res.status(400).send({message: "Bad request", error: err})
-  })
-})
+  let token = auth.decode_token(req.get("x-api-key"));
+  req.token_data = token;
+  dashboard
+    .user_data(req)
+    .then(data => {
+      req.basic_data = data;
+      next();
+    })
+    .catch(err => {
+      return res.status(400).send({ message: "Bad request", error: err });
+    });
+});
 
 //  Middleware for email_verified
 app.all("/*", (req, res, next) => {
   if (
-    req.path == '/login'
-    || req.path == '/'
-    || req.path == '/favicon.ico'
-    || req.path == '/robots.txt'
-    || req.path == '/signup') {
+    req.path == "/login" ||
+    req.path == "/" ||
+    req.path == "/favicon.ico" ||
+    req.path == "/robots.txt" ||
+    req.path == "/signup"
+  ) {
     return next();
   }
   // TODO: add common code for getting data form user table
-  let token_email = auth.decode_token(req.get('x-api-key')).user.email
-  query(`SELECT * FROM users WHERE users.email=?`,token_email).then(data => {
-    if(data.email_verified){
-      next()
-    }else{
-      res.status(400).send({message: "Email_not_verified", error: {}})
-    }
-  }).catch(err => {
-    res.status(500).send({message: "Bad requets", error:err})
-  })
-})
+  let token_email = auth.decode_token(req.get("x-api-key")).user.email;
+  query(`SELECT * FROM users WHERE users.email=?`, token_email)
+    .then(data => {
+      if (data.email_verified) {
+        next();
+      } else {
+        res.status(400).send({ message: "Email_not_verified", error: {} });
+      }
+    })
+    .catch(err => {
+      res.status(500).send({ message: "Bad requets", error: err });
+    });
+});
 
 // Middleware for data1
 app.all("/*", (req, res, next) => {
   if (
-    req.path == '/login'
-    || req.path == '/'
-    || req.path == '/favicon.ico'
-    || req.path == '/robots.txt'
-    || req.path == '/signup'
-    || req.path == '/recruiter/hr'
-    || req.path == '/dashboard'
-    || req.path == '/user'
-    || req.path == '/college/base'
-    || req.path == '/college/coordinator') {
+    req.path == "/login" ||
+    req.path == "/" ||
+    req.path == "/favicon.ico" ||
+    req.path == "/robots.txt" ||
+    req.path == "/signup" ||
+    req.path == "/recruiter/hr" ||
+    req.path == "/dashboard" ||
+    req.path == "/user" ||
+    req.path == "/college/base" ||
+    req.path == "/college/coordinator"
+  ) {
     return next();
   }
   // TODO: add common code for getting data form user table
-  let token_email = auth.decode_token(req.get('x-api-key')).user.email
-  query(`SELECT * FROM users WHERE users.email=?`,token_email).then(data => {
-    if(data.data1){
-      next()
-    }else{
-      res.status(400).send({message: "personal info not found", error: {}})
-    }
-  }).catch(err => {
-    res.status(500).send({message: "Bad requets", error:err})
-  })
-})
+  let token_email = auth.decode_token(req.get("x-api-key")).user.email;
+  query(`SELECT * FROM users WHERE users.email=?`, token_email)
+    .then(data => {
+      if (data.data1) {
+        next();
+      } else {
+        res.status(400).send({ message: "personal info not found", error: {} });
+      }
+    })
+    .catch(err => {
+      res.status(500).send({ message: "Bad requets", error: err });
+    });
+});
 
 // Middleware for data2
 app.all("/*", (req, res, next) => {
   if (
-    req.path == '/login'
-    || req.path == '/'
-    || req.path == '/favicon.ico'
-    || req.path == '/robots.txt'
-    || req.path == '/signup'
-    || req.path == '/recruiter/hr'
-    || req.path == '/recruiter/base'
-    || req.path == '/college/base'
-    || req.path == '/college/coordinator') {
+    req.path == "/login" ||
+    req.path == "/" ||
+    req.path == "/favicon.ico" ||
+    req.path == "/robots.txt" ||
+    req.path == "/signup" ||
+    req.path == "/recruiter/hr" ||
+    req.path == "/recruiter/base" ||
+    req.path == "/college/base" ||
+    req.path == "/college/coordinator"
+  ) {
     return next();
   }
   // TODO: add common code for getting data form user table
-  let token_email = auth.decode_token(req.get('x-api-key')).user.email
-  query(`SELECT * FROM users WHERE users.email=?`,token_email).then(data => {
-    if(data.email_verified){
-      next()
-    }else{
-      res.status(400).send({message: "Corporation info not found", error: {}})
-    }
-  }).catch(err => {
-    res.status(500).send({message: "Bad requets", error:err})
-  })
-})
+  let token_email = auth.decode_token(req.get("x-api-key")).user.email;
+  query(`SELECT * FROM users WHERE users.email=?`, token_email)
+    .then(data => {
+      if (data.email_verified) {
+        next();
+      } else {
+        res
+          .status(400)
+          .send({ message: "Corporation info not found", error: {} });
+      }
+    })
+    .catch(err => {
+      res.status(500).send({ message: "Bad requets", error: err });
+    });
+});
 
 //app.use(expressValidator());
 
@@ -157,4 +168,10 @@ app.all("/*", (req, res, next) => {
 //     })(req, res, next);
 // });
 
-export default app
+app.use("/", base);
+app.use("/autofill", autofill);
+app.use("/college", college);
+app.use("/recruiter", recruiter);
+app.use("/student", student);
+
+export default app;
--- modules/common/dashboard.js
+++ modules/common/dashboard.js
@@ -1,10 +1,10 @@
 import { query } from "../../config/db";
 
 export let dashboard = {
-  user_data: (req) =>{
-    let token_email = req.token_data.user.email
-    let user_type = req.token_data.user.user_type_id
-    let sql
+  user_data: req => {
+    let token_email = req.token_data.user.email;
+    let user_type = req.token_data.user.user_type_id;
+    let sql;
     // TODO: update according to mapping table
     let sql_1 = `SELECT m.recruiter_hr_id As hr_id , r.id As recruiter_id, r.name As recruiter_name, hr.email
                 FROM recruiter r
@@ -14,7 +14,7 @@ export let dashboard = {
                 INNER
                 JOIN recruiter_hr hr
                 ON hr.id = m.recruiter_hr_id
-                where hr.email = ?`
+                where hr.email = ?`;
 
     let sql_2 = `SELECT cc.id As coordinator_id, c.id As college_id, c.name As college_name, cc.email
                 FROM college c
@@ -24,26 +24,28 @@ export let dashboard = {
                 INNER
                 JOIN college_coordinator cc
                 ON cc.id = m.coordinator_id
-                WHERE cc.email = ?`
-    let sql_3
+                WHERE cc.email = ?`;
+    let sql_3;
     switch (user_type) {
       case 1:
-        sql = sql_1
+        sql = sql_1;
         break;
       case 2:
-        sql = sql_2
+        sql = sql_2;
         break;
       case 3:
-        sql = sql_3
+        sql = sql_3;
         break;
 
       default:
         break;
     }
-    return query(sql, token_email).then(data => {
-      return data
-    }).catch(err => {
-      return err
-    })
+    return query(sql, token_email)
+      .then(data => {
+        return data;
+      })
+      .catch(err => {
+        return err;
+      });
   }
-}
+};
